fc60b80364ac1f06c89112bd6b1573868d80c4ee
chintanp <chintan.pathak@gmail.com>
2015-08-12 14:48:50 +0530

    Graph with d3 not working

--- client/app/scripts/directives/graph.js
+++ client/app/scripts/directives/graph.js
@@ -1,4 +1,4 @@
-angular.module('prep').directive('ghVisualization', function () {
+angular.module('prep').directive('ghVisualization', function ($rootScope) {
 
   // constants
   var margin = 20,
@@ -8,193 +8,82 @@ angular.module('prep').directive('ghVisualization', function () {
 
   return {
     restrict: 'E',
-    scope: {
-      val: '=',
-      grouped: '='
-    },
+
     link: function (scope, element, attrs) {
 
-      // set up initial svg object
-      var vis = d3.select(element[0])
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height + margin + 100);
+      var yaw=0.5,pitch=0.5, width=800 , height=300, drag=false;
 
-      scope.$watch('val', function (newVal, oldVal) {
-
-        // clear the elements inside of the directive
-        vis.selectAll('*').remove();
-
-        // if 'val' is undefined, exit
-        if (!newVal) {
-          return;
-        }
-
-        // Based on: http://mbostock.github.com/d3/ex/stack.html
-        var n = newVal.length, // number of layers
-          m = newVal[0].length, // number of samples per layer
-          data = d3.layout.stack()(newVal);
-
-        var mx = m,
-          my = d3.max(data, function(d) {
-            return d3.max(d, function(d) {
-              return d.y0 + d.y;
-            });
-          }),
-          mz = d3.max(data, function(d) {
-            return d3.max(d, function(d) {
-              return d.y;
-            });
-          }),
-          x = function(d) { return d.x * width / mx; },
-          y0 = function(d) { return height - d.y0 * height / my; },
-          y1 = function(d) { return height - (d.y + d.y0) * height / my; },
-          y2 = function(d) { return d.y * height / mz; }; // or `my` not rescale
-
-        // Layers for each color
-        // =====================
-
-        var layers = vis.selectAll("g.layer")
-          .data(data)
-          .enter().append("g")
-          .style("fill", function(d, i) {
-            return color(i / (n - 1));
-          })
-          .attr("class", "layer");
-
-        // Bars
-        // ====
-
-        var bars = layers.selectAll("g.bar")
-          .data(function(d) { return d; })
-          .enter().append("g")
-          .attr("class", "bar")
-          .attr("transform", function(d) {
-            return "translate(" + x(d) + ",0)";
-          });
-
-        bars.append("rect")
-          .attr("width", x({x: .9}))
-          .attr("x", 0)
-          .attr("y", height)
-          .attr("height", 0)
-          .transition()
-          .delay(function(d, i) { return i * 10; })
-          .attr("y", y1)
-          .attr("height", function(d) {
-            return y0(d) - y1(d);
-          });
-
-        // X-axis labels
-        // =============
-
-        var labels = vis.selectAll("text.label")
-          .data(data[0])
-          .enter().append("text")
-          .attr("class", "label")
-          .attr("x", x)
-          .attr("y", height + 6)
-          .attr("dx", x({x: .45}))
-          .attr("dy", ".71em")
-          .attr("text-anchor", "middle")
-          .text(function(d, i) {
-            return d.date;
-          });
-
-        // Chart Key
-        // =========
-
-        var keyText = vis.selectAll("text.key")
-          .data(data)
-          .enter().append("text")
-          .attr("class", "key")
-          .attr("y", function (d, i) {
-            return height + 42 + 30*(i%3);
-          })
-          .attr("x", function (d, i) {
-            return 155 * Math.floor(i/3) + 15;
-          })
-          .attr("dx", x({x: .45}))
-          .attr("dy", ".71em")
-          .attr("text-anchor", "left")
-          .text(function(d, i) {
-            return d[0].user;
-          });
-
-        var keySwatches = vis.selectAll("rect.swatch")
-          .data(data)
-          .enter().append("rect")
-          .attr("class", "swatch")
-          .attr("width", 20)
-          .attr("height", 20)
-          .style("fill", function(d, i) {
-            return color(i / (n - 1));
-          })
-          .attr("y", function (d, i) {
-            return height + 36 + 30*(i%3);
-          })
-          .attr("x", function (d, i) {
-            return 155 * Math.floor(i/3);
-          });
-
-
-        // Animate between grouped and stacked
-        // ===================================
-
-        function transitionGroup() {
-          vis.selectAll("g.layer rect")
-            .transition()
-            .duration(500)
-            .delay(function(d, i) { return (i % m) * 10; })
-            .attr("x", function(d, i) { return x({x: .9 * ~~(i / m) / n}); })
-            .attr("width", x({x: .9 / n}))
-            .each("end", transitionEnd);
-
-          function transitionEnd() {
-            d3.select(this)
-              .transition()
-              .duration(500)
-              .attr("y", function(d) { return height - y2(d); })
-              .attr("height", y2);
+      function dataFromFormular(func){
+        var output=[];
+        for(var x=-20;x<20;x++){
+          var f0=[];
+          output.push(f0);
+          for(var y=-20;y<20;y++){
+            f0.push(func(x,y));
           }
         }
+        return output;
+      }
 
-        function transitionStack() {
-          vis.selectAll("g.layer rect")
-            .transition()
-            .duration(500)
-            .delay(function(d, i) { return (i % m) * 10; })
-            .attr("y", y1)
-            .attr("height", function(d) {
-              return y0(d) - y1(d);
-            })
-            .each("end", transitionEnd);
-
-          function transitionEnd() {
-            d3.select(this)
-              .transition()
-              .duration(500)
-              .attr("x", 0)
-              .attr("width", x({x: .9}));
-          }
+      var surfaces=[
+        {
+          name: 'Dataset 1',
+          data: dataFromFormular(function(x,y){
+            return (Math.sqrt(x*x+y*y - 50));
+          })
         }
+      ];
+      var selected=surfaces[0];
+      //d3.selectAll().remove();
 
-        // reset grouped state to false
-        scope.grouped = false;
+      var ul=d3.select('body')
+        .append('ul');
+      var svg=d3.select('body')
+        .append('svg')
+        .attr('height',height)
+        .attr('width',width);
 
-        // setup a watch on 'grouped' to switch between views
-        scope.$watch('grouped', function (newVal, oldVal) {
-          // ignore first call which happens before we even have data from the Github API
-          if (newVal === oldVal) {
-            return;
-          }
-          if (newVal) {
-            transitionGroup();
-          } else {
-            transitionStack();
-          }
+      var group = svg.append("g");
+
+      var md=group.data([surfaces[0].data])
+        .surface3D(width,height)
+        .surfaceHeight(function(d){
+          return d;
+        }).surfaceColor(function(d){
+          var c=d3.hsl((d+100), 0.6, 0.5).rgb();
+          return "rgb("+parseInt(c.r)+","+parseInt(c.g)+","+parseInt(c.b)+")";
         });
+
+      /*ul.selectAll('li')
+        .data(surfaces)
+        .enter().append('li')
+        .html(function(d){
+          return d.name
+        }).on('mousedown',function(){
+          md.data([d3.select(this).datum().data]).surface3D()
+            .transition().duration(500)
+            .surfaceHeight(function(d){
+              return d;
+            }).surfaceColor(function(d){
+              var c=d3.hsl((d+100), 0.6, 0.5).rgb();
+              return "rgb("+parseInt(c.r)+","+parseInt(c.g)+","+parseInt(c.b)+")";
+            });
+        });*/
+
+      svg.on("mousedown",function(){
+        drag=[d3.mouse(this),yaw,pitch];
+      }).on("mouseup",function(){
+        drag=false;
+      }).on("mousemove",function(){
+        if(drag){
+          var mouse=d3.mouse(this);
+          yaw=drag[1]-(mouse[0]-drag[0][0])/50;
+          pitch=drag[2]+(mouse[1]-drag[0][1])/50;
+          pitch=Math.max(-Math.PI/2,Math.min(Math.PI/2,pitch));
+          md.turntable(yaw,pitch);
+        }
       });
+
     }
   }
 });
