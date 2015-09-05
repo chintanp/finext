1b901f1bd310c8b17a633ac73d59fe3a874cc801
chintanp <chintan.pathak@gmail.com>
2015-09-05 19:02:57 +0530

    WIP 3D surface plot using visjs

--- client/app/scripts/directives/graph.js
+++ client/app/scripts/directives/graph.js
@@ -13,7 +13,45 @@ angular.module('prep').directive('ghVisualization', function () {
 
     controller: function ($scope, $element) {
 
-      $scope.changeText = function(data){
+      // Create and populate a data table.
+      var data = new vis.DataSet();
+      // create some nice looking data with sin/cos
+      var counter = 0;
+      var steps_x = $scope.model.divx; //50;  // number of datapoints will be steps*steps
+      var steps_y = $scope.model.divy;
+
+      var axis_x_Max = $scope.model.length; //314;
+      var axis_y_Max = $scope.model.breadth;
+
+      var axis_x_Step = axis_x_Max / steps_x; // axisMax / steps;
+      var axis_y_Step = axis_y_Max / steps_y;
+
+      for (var x = 0; x <= axis_x_Max; x += axis_x_Step) {
+        for (var y = 0; y <= axis_y_Max; y += axis_y_Step) {
+          var value = (Math.sin(x/50) * Math.cos(y/50) * 50 + 50);
+          data.add({id:counter++,x:x,y:y,z:value,style:value});
+        }
+      }
+
+      // specify options
+      var options = {
+        width:  '500px',
+        height: '552px',
+        style: 'surface',
+        showPerspective: true,
+        showGrid: true,
+        showShadow: false,
+        keepAspectRatio: true,
+        verticalRatio: 0.5
+      };
+
+      // Instantiate our graph object.
+      var container = document.getElementById('chart');
+      var graph3d = new vis.Graph3d(container, data, options);
+
+
+
+    /*  $scope.changeText = function(data){
         $scope.text = 'New directive text';
         $scope.data = data;
 
@@ -21,10 +59,10 @@ angular.module('prep').directive('ghVisualization', function () {
 
         function dataFromFormular(func){
           var output=[];
-          for(var x=-30;x<30;x++){
+          for(var x = -1 * $scope.data.inputs.divx; x<$scope.data.inputs.divx; x++){
             var f0=[];
             output.push(f0);
-            for(var y=-10;y<15;y++){
+            for(var y= -1 * $scope.data.inputs.divy; y < $scope.data.inputs.divy; y++){
               f0.push(func(x,y));
             }
           }
@@ -42,8 +80,8 @@ angular.module('prep').directive('ghVisualization', function () {
         var selected=surfaces[0];
         //d3.selectAll().remove();
 
-        /*var ul=d3.select('body')
-         .append('ul');*/
+        /!*var ul=d3.select('body')
+         .append('ul');*!/
         var svg=d3.select('#chart')
           .append('svg')
           .attr('height',height)
@@ -60,7 +98,7 @@ angular.module('prep').directive('ghVisualization', function () {
             return "rgb("+parseInt(c.r)+","+parseInt(c.g)+","+parseInt(c.b)+")";
           });
 
-        /*ul.selectAll('li')
+        /!*ul.selectAll('li')
          .data(surfaces)
          .enter().append('li')
          .html(function(d){
@@ -74,7 +112,7 @@ angular.module('prep').directive('ghVisualization', function () {
          var c=d3.hsl((d+100), 0.6, 0.5).rgb();
          return "rgb("+parseInt(c.r)+","+parseInt(c.g)+","+parseInt(c.b)+")";
          });
-         });*/
+         });*!/
 
         svg.on("mousedown",function(){
           drag=[d3.mouse(this),yaw,pitch];
@@ -96,7 +134,7 @@ angular.module('prep').directive('ghVisualization', function () {
       $scope.$on('changeText',function(event, data){
         $scope.changeText(data)
       });
-
+*/
 
     }
   }
