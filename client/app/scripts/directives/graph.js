ebcabd12ae5a197b8adc38f3956f996e0022633a
chintanp <chintan.pathak@gmail.com>
2015-09-06 11:16:09 +0530

    WIP graph, works a lil bit for 4 node

--- client/app/scripts/directives/graph.js
+++ client/app/scripts/directives/graph.js
@@ -13,43 +13,58 @@ angular.module('prep').directive('ghVisualization', function () {
 
     controller: function ($scope, $element) {
 
-      // Create and populate a data table.
-      var data = new vis.DataSet();
-      // create some nice looking data with sin/cos
-      var counter = 0;
-      var steps_x = $scope.model.divx; //50;  // number of datapoints will be steps*steps
-      var steps_y = $scope.model.divy;
+      $scope.changeText = function(data_from_server) {
 
-      var axis_x_Max = $scope.model.length; //314;
-      var axis_y_Max = $scope.model.breadth;
+        // Create and populate a data table.
+        var data = new vis.DataSet();
+        // create some nice looking data with sin/cos
+        var counter = 0;
+        var steps_x = $scope.model.divx; //50;  // number of datapoints will be steps*steps
+        var steps_y = $scope.model.divy;
 
-      var axis_x_Step = axis_x_Max / steps_x; // axisMax / steps;
-      var axis_y_Step = axis_y_Max / steps_y;
+        var axis_x_Max = $scope.model.length; //314;
+        var axis_y_Max = $scope.model.breadth;
 
-      for (var x = 0; x <= axis_x_Max; x += axis_x_Step) {
-        for (var y = 0; y <= axis_y_Max; y += axis_y_Step) {
-          var value = (Math.sin(x/50) * Math.cos(y/50) * 50 + 50);
-          data.add({id:counter++,x:x,y:y,z:value,style:value});
+        var axis_x_Step = axis_x_Max / steps_x; // axisMax / steps;
+        var axis_y_Step = axis_y_Max / steps_y;
+
+        if(data_from_server.inputs.element_type == 1) {
+          for (var i = 0;  i <= steps_x; i++) {
+            for (var j = 0; j <= steps_y; j++) {
+              var value = data_from_server.w[i*(steps_y+1)+j]; //(Math.sin(x / 50) * Math.cos(y / 50) * 50 + 50);
+              data.add({id: counter++, x: i*axis_x_Step, y: j*axis_y_Step, z: value, style: value});
+            }
+          }
+
+          /*for (var x = 0, i = 0; x <= axis_x_Max, i <= steps_x; x += axis_x_Step, i++) {
+            for (var y = 0, j = 0; y <= axis_y_Max, j <= steps_y; y += axis_y_Step, j++) {
+              var value = data_from_server.w[i][j]//(Math.sin(x / 50) * Math.cos(y / 50) * 50 + 50);
+              data.add({id: counter++, x: x, y: y, z: value, style: value});
+            }
+          }*/
         }
+
+
+        // specify options
+        var options = {
+          width: '500px',
+          height: '552px',
+          style: 'surface',
+          showPerspective: true,
+          showGrid: true,
+          showShadow: false,
+          keepAspectRatio: true,
+          verticalRatio: 0.5
+        };
+
+        // Instantiate our graph object.
+        var container = document.getElementById('chart');
+        var graph3d = new vis.Graph3d(container, data, options);
       }
 
-      // specify options
-      var options = {
-        width:  '500px',
-        height: '552px',
-        style: 'surface',
-        showPerspective: true,
-        showGrid: true,
-        showShadow: false,
-        keepAspectRatio: true,
-        verticalRatio: 0.5
-      };
-
-      // Instantiate our graph object.
-      var container = document.getElementById('chart');
-      var graph3d = new vis.Graph3d(container, data, options);
-
-
+      $scope.$on('changeText',function(event, data){
+        $scope.changeText(data)
+      })
 
     /*  $scope.changeText = function(data){
         $scope.text = 'New directive text';
